fe76b80e7ac870dd34ab02eb7047bbf90e28269d
Alexey_wl <allex_doma@mail.ru>
2022-11-26 15:05:22 +0800

    добавил 5 задачу

--- 5.py
+++ 5.py
@@ -1,12 +1,18 @@
 # Реализуйте алгоритм перемешивания списка.
 import random
 
-n = int(input("Введите размер списка "))
-chain = list(map(int, input(f'Введите через пробел {n} элемента(-ов) списка ').split(' ')))
-print(chain)
-new_chain = []
-while len(chain) > 1:
-    new_chain.append(chain.pop(random.randint(0, len(chain) - 1)))
-new_chain.append(chain.pop())
-print(new_chain)
+def mix_chain(chain):
+    new_chain = []
+    while len(chain) > 1:
+        new_chain.append(chain.pop(random.randint(0, len(chain) - 1)))
+    new_chain.append(chain.pop())
+    return new_chain
 
+
+n = int(input("Введите размер списка "))
+chain = list(
+    map(int, input(f'Введите через пробел {n} элемента(-ов) списка ').split(' ')))
+print(chain)
+
+
+print(mix_chain(chain))
